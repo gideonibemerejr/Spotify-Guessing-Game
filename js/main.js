f29defefd34f64b687b24fcdf9c805ff100ac06e
Gideon Ibemere Jr <goij@Gideons-MacBook-Pro.local>
2019-05-03 11:18:29 -0500

    Add pseudocode for index.html and main.j

---
+++ js/main.js
@@ -0,0 +1,42 @@
+class Artist = {
+    constructor(name, song, country, spotify) {
+        this.name = name;
+        this.song = song;
+        this.country = country;
+        this.spotify = spotify;
+    }
+    mountSong(){
+
+    }
+}
+
+const Skepta = new Artist('Skepta', "")
+
+/*----- constants -----*/
+// Create an Artist Class that initializes the following properties:
+    // name -- artist's name
+    // song -- song link for Spotify play button
+    // emoji -- country that the artist is from
+    // Spotify artist url -- link to spotify information for the artist
+    // mountSong method 
+        // get the Artist.song (song link for Spotify play button)
+        // change the href in the Spotify Play Button to Artist.song
+        // change feedback to "Make sure you listen to the whole song"
+        // show disabled 
+    // promptGuess
+        // after 20 seconds change the feedback to ask where are they from?
+        // enable controls
+        
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+// Game Title
+// Game "Board"
+// The Player
+// The Artist
+// Feedback
+// Input
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
