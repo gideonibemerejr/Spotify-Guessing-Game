47c3d307678d02fd93db68e43edec9334415d5f7
Gideon Ibemere Jr <goij@Gideons-MacBook-Pro.local>
2019-05-08 09:52:32 -0500

    Update main.js -- iframe situation is not complete

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@
 
 // Artist Class that initializes the following properties:
 class Artist {
-  constructor(name, country, link) {
+  constructor(name, country, link, bg, tc) {
     // name -- artist's name
     this.name = name;
     // country -- country that the artist is from
     this.country = country;
     // link -- song link for Spotify play button
     this.link = link;
+    // background gradient
+    this.bg = bg;
+    // text color
+    this.tc = tc;
   }
   mountSong() {
     return this.link;
@@ -18,32 +22,44 @@ class Artist {
 const skepta = new Artist(
     "Skepta",
     "England",
-    `<iframe src="https://open.spotify.com/embed/track/1ATVSVN4kc8S2XE7FdyJi8" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
+    `<iframe src="https://open.spotify.com/embed/track/1ATVSVN4kc8S2XE7FdyJi8" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`,
+    ``,
+    ``
   ),
   burna = new Artist(
     "Burna Boy",
     "Nigeria",
-    `<iframe src="https://open.spotify.com/embed/track/3FskQrDXcY24ur2fCvz35O" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
+    `<iframe src="https://open.spotify.com/embed/track/3FskQrDXcY24ur2fCvz35O" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`,
+    ``,
+    ``
   ),
   roddy = new Artist(
     "Roddy Ricch",
     "USA",
-    `<iframe src="https://open.spotify.com/embed/track/3wLtYwtqvKK2ewelSVPeDK" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
+    `<iframe src="https://open.spotify.com/embed/track/3wLtYwtqvKK2ewelSVPeDK" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`,
+    ``,
+    ``
   ),
   vegedream = new Artist(
     "Vegedream",
     "France",
-    `<iframe src="https://open.spotify.com/embed/track/1m0UFnuTktOkksvjbF9z0m" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
+    `<iframe src="https://open.spotify.com/embed/track/1m0UFnuTktOkksvjbF9z0m" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`,
+    ``,
+    ``
   ),
   chambao = new Artist(
     "Chambao",
     "Spain",
-    `<iframe src="https://open.spotify.com/embed/track/2HwoPy0HkKLQaJ6ONv1pmy" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
+    `<iframe src="https://open.spotify.com/embed/track/2HwoPy0HkKLQaJ6ONv1pmy" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`,
+    ``,
+    ``
   ),
   dkvpz = new Artist(
     "Dkvpz",
     "Brazil",
-    `<iframe src="https://open.spotify.com/embed/track/4CnVCvv9e5DHrOWXvNbJV1" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
+    `<iframe src="https://open.spotify.com/embed/track/4CnVCvv9e5DHrOWXvNbJV1" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`,
+    ``,
+    ``
   );
 // Array of artist objects to access
 const artists = [skepta, burna, roddy, vegedream, chambao, dkvpz];
@@ -63,7 +79,7 @@ let playedArtists = [];
 
 /*----- app's state (variables) -----*/
 
-let result, score, artist, button;
+let result, score, artist, button, player;
 
 /*----- cached element references -----*/
 // Game "Board"
@@ -72,7 +88,6 @@ const playerHolder = document.getElementById("content"),
   // The Artist title
   artistTitle = document.getElementById("artist__name"),
   // The Audio Player
-  player = document.querySelector("iframe"),
   countdownText = document.getElementById("countdown"),
   // Feedback
   feedback = document.getElementById("feedback"),
@@ -87,9 +102,9 @@ document.getElementById("button").addEventListener("click", buttonClickHandler);
 // Input - 'click'/'select'
 input.addEventListener("click", inputClickHandler);
 
-//iframe - 'click'
-
+// iframe
 /*----- functions -----*/
+
 // init -- empties playedArtists, randomly select an Artist from artists[],
 function init() {
   playedArtists = [];
@@ -98,9 +113,21 @@ function init() {
   artist = getArtist();
 }
 
+// mountSong -- method to return this.song
+
+// Counter -- counts down and then runs promptGuess
+
+// promptGuess -- function to remove board and ask for guess
+
+// mountButton -- change button to the target and render the button
 function mountButton(targetButton) {
   button = targetButton;
   render(targetButton);
+}
+
+// getArtist -- return artist
+function getArtist() {
+  return (artist = artists[Math.floor(Math.random() * artists.length)]);
 }
 
 // render -- takes in artist, trackLink, button,  shows song iframe,  and artist's name + press play feedback
@@ -139,8 +166,12 @@ function render(targetButton) {
     countdownText.style.display = "none";
     playerHolder.innerHTML = artist.mountSong();
     feedback.style.display = "block";
-    button.id = "Continue";
+    player = document.querySelector("iframe").contentDocument;
+    player.body.onmousedown = iframeClickHandler;
     return;
+  }
+  if (targetButton.id === "continue") {
+    console.log(player);
   }
 }
 // countdown -- counts user down from 5 and runs mountSong
@@ -165,18 +196,13 @@ function buttonClickHandler(e) {
       console.log("wtf");
   }
 }
+
 // when input is clicked
 function inputClickHandler(e) {
   console.log(e.target);
 }
 
-// getArtist -- return artist
-function getArtist() {
-  return (artist = artists[Math.floor(Math.random() * artists.length)]);
+// when iframe is clicked
+function iframeClickHandler(e) {
+  console.log(e.target);
 }
-
-// mountSong -- method to return this.song
-
-// Counter -- counts down and then runs promptGuess
-
-// promptGuess -- function to remove board and ask for guess
